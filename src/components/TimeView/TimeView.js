8ae330a151356eaf828649df18259dc479725252
Иван Недзвецкий <stels-cs@yandex.ru>
2020-01-16 13:59:13 +0300

    Минуты показывались без ведущего нуля

--- src/components/TimeView/TimeView.js
+++ src/components/TimeView/TimeView.js
@@ -1,18 +1,18 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 
-const months_ru = ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек']
-
 export default class TimeView extends Component {
-
 	render() {
 		const {timestamp} = this.props
 		const time = new Date(timestamp * 1000)
 		let year = time.getFullYear()
-		let month = months_ru[time.getMonth()]
+		let month = this.props.month[time.getMonth()]
 		let date = time.getDate()
 		let hour = time.getHours()
 		let min = time.getMinutes()
+		if (min < 10) {
+			min = '0' + min
+		}
 		let now = new Date()
 		if (year === now.getFullYear()) {
 			return date + ' ' + month + ' ' + hour + ':' + min
@@ -25,9 +25,11 @@ export default class TimeView extends Component {
 TimeView.propTypes = {
 	/** Время в секундах */
 	timestamp: PropTypes.number,
-	lang: PropTypes.string
+	lang: PropTypes.string,
+	month: PropTypes.array,
 }
 
 TimeView.defaultProps = {
-	lang: 'ru'
+	lang: 'ru',
+	month: ['янв', 'фев', 'мар', 'апр', 'май', 'июн', 'июл', 'авг', 'сен', 'окт', 'ноя', 'дек'],
 }
